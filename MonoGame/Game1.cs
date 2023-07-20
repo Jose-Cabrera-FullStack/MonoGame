4ef3c9c23889a516c01f6d847e837b13c084cacd
Jose Cabrera <murdoc.jose.6@gmail.com>
2023-07-19 23:06:05 -0300

    render lucas duck in middle of the screen

--- MonoGame/Game1.cs
+++ MonoGame/Game1.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Flat.Graphics;
 
 namespace MonoGame
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager graphics;
+        private Sprites sprites;
+        private Texture2D texture;
 
         public Game1()
         {
@@ -21,12 +24,14 @@ namespace MonoGame
         protected override void Initialize()
         {
 
+            this.sprites = new Sprites(this);
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-
+            this.texture = this.Content.Load<Texture2D>("Duck");
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,6 +48,12 @@ namespace MonoGame
         protected override void Draw(GameTime gameTime)
         {
             this.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            Viewport viewport = this.GraphicsDevice.Viewport;
+
+            this.sprites.Begin(false);
+            this.sprites.Draw(texture, Vector2.Zero, new Vector2(viewport.Width / 2, viewport.Height / 2), Color.White);
+            this.sprites.End();
 
             base.Draw(gameTime);
         }
