cb1e65b37c78e241f91efa7ee942e62bf7b65720
Jose Cabrera <murdoc.jose.6@gmail.com>
2024-03-07 00:07:23 -0300

    feat: opened window feature done

--- Physics/Game1.cs
+++ Physics/Game1.cs
@@ -1,50 +1,89 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Flat;
+using Flat.Graphics;
+using Flat.Input;
+
 
 namespace Physics;
 
 public class Game1 : Game
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
+    private GraphicsDeviceManager graphics;
+    private Screen screen;
+    private Sprites sprites;
+    private Shapes shapes;
+    private Camera camera;
 
     public Game1()
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+        this.graphics = new GraphicsDeviceManager(this);
+        this.graphics.SynchronizeWithVerticalRetrace = true;
+
+        this.Content.RootDirectory = "Content";
+        this.IsMouseVisible = true;
+        this.IsFixedTimeStep = true;
+
+        const double UpdatesPerSecond = 60d;
+        this.TargetElapsedTime = TimeSpan.FromTicks((long)Math.Round((double)TimeSpan.TicksPerSecond / UpdatesPerSecond));
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
+        FlatUtil.SetRelativeBackBufferSize(this.graphics, 0.85f);
+
+        this.screen = new Screen(this, 1280, 720);
+        this.sprites = new Sprites(this);
+        this.shapes = new Shapes(this);
+        this.camera = new Camera(this.screen);
+        this.camera.Zoom = 5;
 
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        FlatKeyboard keyboard = FlatKeyboard.Instance;
+        FlatMouse mouse = FlatMouse.Instance;
 
-        // TODO: Add your update logic here
+        keyboard.Update();
+        mouse.Update();
+
+        if (keyboard.IsKeyDown(Keys.Escape))
+        {
+            this.Exit();
+        }
+
+        if (keyboard.IsKeyDown(Keys.A))
+        {
+            this.camera.IncZoom();
+        }
+
+        if (keyboard.IsKeyDown(Keys.Z))
+        {
+            this.camera.DecZoom();
+        }
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        this.screen.Set();
+        GraphicsDevice.Clear(new Color(50, 60, 70));
 
-        // TODO: Add your drawing code here
+        this.shapes.Begin(this.camera);
+        this.shapes.DrawCircle(0, 0, 32, 32, Color.White);
+        this.shapes.End();
+
+        this.screen.Unset();
+        this.screen.Present(this.sprites);
 
         base.Draw(gameTime);
     }
